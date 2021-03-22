1c20f57cffaf278f8710c4e390757e5c57c26412
MacBook <macbook@MacBook-Pro-MacBook.local>
2021-03-22 22:25:11 +0200

    add form send server mamb, get post

--- js/script.js
+++ js/script.js
@@ -214,6 +214,59 @@ document.addEventListener('DOMContentLoaded', () => {
         20,
         '.menu .container'
     ).render();
+
+    //Forms 
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        succes: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json')
+            const formDate = new FormData(form);
+
+            const obj = {};
+            formDate.forEach(function (value, key) {
+                obj[key] = value;
+            });
+
+            const json = JSON.stringify(obj);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.succes;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
+
 }); //end
 
  
